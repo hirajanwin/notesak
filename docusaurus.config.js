b6112598c936e24d362d7de982997073824a697a
akormous <iamakshatchauhan@gmail.com>
2022-12-20 14:07:10 +0530

    Update edit URL

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,14 +44,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/akormous/notes/edit/master/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/akormous/notes/edit/master/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
